0af79977529d6c3fcd7cf8f233fa123354569c62
Hamed Salameh <hamed.salameh@outlook.com>
2018-02-01 13:50:41 +0200

    Initial creation

---
+++ GreenPrintClient/GreenPrintClient/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace GreenPrintClient
+{
+    partial class formGreenPrintClientMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtDocumentName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtClientID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtAddCC = new System.Windows.Forms.TextBox();
+            this.btnAddToList = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstCCList = new System.Windows.Forms.ListBox();
+            this.rbRemoteSign = new System.Windows.Forms.RadioButton();
+            this.rbDeviceSign = new System.Windows.Forms.RadioButton();
+            this.txtEmailAddress = new System.Windows.Forms.TextBox();
+            this.cbRecipientEmail = new System.Windows.Forms.CheckBox();
+            this.txtSMSNumber = new System.Windows.Forms.TextBox();
+            this.cbRecipientSMS = new System.Windows.Forms.CheckBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtDocumentName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtClientID);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(365, 80);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "General Details";
+            // 
+            // txtDocumentName
+            // 
+            this.txtDocumentName.Location = new System.Drawing.Point(99, 51);
+            this.txtDocumentName.Name = "txtDocumentName";
+            this.txtDocumentName.Size = new System.Drawing.Size(238, 20);
+            this.txtDocumentName.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Document Name";
+            // 
+            // txtClientID
+            // 
+            this.txtClientID.Location = new System.Drawing.Point(99, 25);
+            this.txtClientID.Name = "txtClientID";
+            this.txtClientID.Size = new System.Drawing.Size(238, 20);
+            this.txtClientID.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Client ID";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtAddCC);
+            this.groupBox2.Controls.Add(this.btnAddToList);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.lstCCList);
+            this.groupBox2.Controls.Add(this.rbRemoteSign);
+            this.groupBox2.Controls.Add(this.rbDeviceSign);
+            this.groupBox2.Controls.Add(this.txtEmailAddress);
+            this.groupBox2.Controls.Add(this.cbRecipientEmail);
+            this.groupBox2.Controls.Add(this.txtSMSNumber);
+            this.groupBox2.Controls.Add(this.cbRecipientSMS);
+            this.groupBox2.Location = new System.Drawing.Point(12, 98);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(365, 293);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Signing && Delivery options";
+            // 
+            // txtAddCC
+            // 
+            this.txtAddCC.Location = new System.Drawing.Point(15, 158);
+            this.txtAddCC.Name = "txtAddCC";
+            this.txtAddCC.Size = new System.Drawing.Size(252, 20);
+            this.txtAddCC.TabIndex = 28;
+            // 
+            // btnAddToList
+            // 
+            this.btnAddToList.Location = new System.Drawing.Point(277, 155);
+            this.btnAddToList.Name = "btnAddToList";
+            this.btnAddToList.Size = new System.Drawing.Size(75, 23);
+            this.btnAddToList.TabIndex = 27;
+            this.btnAddToList.Text = "Add";
+            this.btnAddToList.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 13);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Send signed document as CC to ...";
+            // 
+            // lstCCList
+            // 
+            this.lstCCList.FormattingEnabled = true;
+            this.lstCCList.Location = new System.Drawing.Point(15, 184);
+            this.lstCCList.Name = "lstCCList";
+            this.lstCCList.Size = new System.Drawing.Size(337, 95);
+            this.lstCCList.TabIndex = 25;
+            // 
+            // rbRemoteSign
+            // 
+            this.rbRemoteSign.AutoSize = true;
+            this.rbRemoteSign.Location = new System.Drawing.Point(9, 56);
+            this.rbRemoteSign.Name = "rbRemoteSign";
+            this.rbRemoteSign.Size = new System.Drawing.Size(133, 17);
+            this.rbRemoteSign.TabIndex = 22;
+            this.rbRemoteSign.Text = "Sign via remote device";
+            this.rbRemoteSign.UseVisualStyleBackColor = true;
+            // 
+            // rbDeviceSign
+            // 
+            this.rbDeviceSign.AutoSize = true;
+            this.rbDeviceSign.Checked = true;
+            this.rbDeviceSign.Location = new System.Drawing.Point(9, 33);
+            this.rbDeviceSign.Name = "rbDeviceSign";
+            this.rbDeviceSign.Size = new System.Drawing.Size(152, 17);
+            this.rbDeviceSign.TabIndex = 21;
+            this.rbDeviceSign.TabStop = true;
+            this.rbDeviceSign.Text = "Sign via connected device";
+            this.rbDeviceSign.UseVisualStyleBackColor = true;
+            // 
+            // txtEmailAddress
+            // 
+            this.txtEmailAddress.Enabled = false;
+            this.txtEmailAddress.Location = new System.Drawing.Point(124, 102);
+            this.txtEmailAddress.Name = "txtEmailAddress";
+            this.txtEmailAddress.Size = new System.Drawing.Size(228, 20);
+            this.txtEmailAddress.TabIndex = 20;
+            // 
+            // cbRecipientEmail
+            // 
+            this.cbRecipientEmail.AutoSize = true;
+            this.cbRecipientEmail.Enabled = false;
+            this.cbRecipientEmail.Location = new System.Drawing.Point(28, 104);
+            this.cbRecipientEmail.Name = "cbRecipientEmail";
+            this.cbRecipientEmail.Size = new System.Drawing.Size(92, 17);
+            this.cbRecipientEmail.TabIndex = 19;
+            this.cbRecipientEmail.Text = "Sign via Email";
+            this.cbRecipientEmail.UseVisualStyleBackColor = true;
+            // 
+            // txtSMSNumber
+            // 
+            this.txtSMSNumber.Enabled = false;
+            this.txtSMSNumber.Location = new System.Drawing.Point(124, 76);
+            this.txtSMSNumber.Name = "txtSMSNumber";
+            this.txtSMSNumber.Size = new System.Drawing.Size(228, 20);
+            this.txtSMSNumber.TabIndex = 18;
+            // 
+            // cbRecipientSMS
+            // 
+            this.cbRecipientSMS.AutoSize = true;
+            this.cbRecipientSMS.Enabled = false;
+            this.cbRecipientSMS.Location = new System.Drawing.Point(28, 79);
+            this.cbRecipientSMS.Name = "cbRecipientSMS";
+            this.cbRecipientSMS.Size = new System.Drawing.Size(90, 17);
+            this.cbRecipientSMS.TabIndex = 17;
+            this.cbRecipientSMS.Text = "Sign via SMS";
+            this.cbRecipientSMS.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(208, 397);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 26;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(289, 397);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 25;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // formGreenPrintClientMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 429);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "formGreenPrintClientMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GreenPrintClient";
+            this.Load += new System.EventHandler(this.formGreenPrintClientMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtDocumentName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtClientID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtAddCC;
+        private System.Windows.Forms.Button btnAddToList;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstCCList;
+        private System.Windows.Forms.RadioButton rbRemoteSign;
+        private System.Windows.Forms.RadioButton rbDeviceSign;
+        private System.Windows.Forms.TextBox txtEmailAddress;
+        private System.Windows.Forms.CheckBox cbRecipientEmail;
+        private System.Windows.Forms.TextBox txtSMSNumber;
+        private System.Windows.Forms.CheckBox cbRecipientSMS;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
---
+++ GreenPrintClient/GreenPrintClient/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GreenPrintClient
+{
+    public partial class formGreenPrintClientMain : Form
+    {
+        public formGreenPrintClientMain()
+        {
+            InitializeComponent();
+        }
+
+        private void formGreenPrintClientMain_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+
+            var path = @"c:\temp";
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = path;
+            watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.Size;
+            watcher.Filter = "*.*";
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized) //<<======Now it wont give exception**
+            {
+                //Load data correspondin to "MyName"
+                //Populate a globale variable List<string> which will be
+                //bound to grid at some later stage
+                if (InvokeRequired)
+                {
+                    // after we've done all the processing, 
+                    this.Invoke(new MethodInvoker(delegate {
+                        // load the control with the appropriate data
+                        this.WindowState = FormWindowState.Normal;
+                    }));
+                    return;
+                }
+            }
+            MessageBox.Show("Change detected!") ;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            
+            this.Close();
+        }
+    }
+}
